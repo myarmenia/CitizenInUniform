35bbd8d2a8628aac3a51cb7047a742ca50486c92
NarineHay <naromisho87@gmail.com>
2024-11-07 12:30:37 +0400

    55555 file if changes

--- app/Http/Requests/SubCategoryRequest.php
+++ app/Http/Requests/SubCategoryRequest.php
@@ -37,25 +37,27 @@ class SubCategoryRequest extends FormRequest
 
     public function withValidator($validator)
     {
-        $validator->after(function ($validator) {
-            // Преобразуем FileBag в массив файлов
-            $files = $this->file('files'); // Используем метод 'file' для получения файлов
+        if(isset($this->files)){
+            $validator->after(function ($validator) {
+                // Преобразуем FileBag в массив файлов
+                $files = $this->file('files'); // Используем метод 'file' для получения файлов
 
-            $totalSize = 0;
+                $totalSize = 0;
 
-            foreach ($files as $file) {
-                // Проверяем, что $file — это объект UploadedFile
-                if ($file instanceof \Symfony\Component\HttpFoundation\File\UploadedFile) {
-                    $totalSize += $file->getSize(); // Получаем размер файла
+                foreach ($files as $file) {
+                    // Проверяем, что $file — это объект UploadedFile
+                    if ($file instanceof \Symfony\Component\HttpFoundation\File\UploadedFile) {
+                        $totalSize += $file->getSize(); // Получаем размер файла
+                    }
                 }
-            }
 
-            $maxTotalSize = 50 * 1024 * 1024; // 50 MB
+                $maxTotalSize = 50 * 1024 * 1024; // 50 MB
 
-            if ($totalSize > $maxTotalSize) {
-                $validator->errors()->add('files', "Բոլոր ֆայլերի ընդհանուր չափը չպետք է գերազանցի 50 մեգաբայթ:");
-            }
-        });
+                if ($totalSize > $maxTotalSize) {
+                    $validator->errors()->add('files', "Բոլոր ֆայլերի ընդհանուր չափը չպետք է գերազանցի 50 մեգաբայթ:");
+                }
+            });
+        }
     }
 
 }
