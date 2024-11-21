9a876254fdb611e58df9e15ec0f7e0023498d9d7
NarineHay <naromisho87@gmail.com>
2024-11-21 11:34:35 +0400

    21.11.24 repport

--- app/Http/Controllers/Api/Reports/ReportController.php
+++ app/Http/Controllers/Api/Reports/ReportController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers\Api\Reports;
 
+use App\DTO\ReportDto;
 use App\Http\Controllers\Controller;
+use App\Services\ReportService;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
+    public function __construct(protected ReportService $service)
+    {
+
+    }
     public function report(Request $request){
-        dd($request->all());
+
+        $data = array_filter($request->all(), fn($value) => $value !== null);
+
+        $data = $this->service->report(ReportDto::fromReportDto($data));
+
+        return $data != null ? $this->sendResponse($data, 'success') : $this->sendError('error');
     }
 }
